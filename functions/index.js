0626e9c4e679c691aa4a69d1d8d6ead66d02297a
Giovanna Radica <g.radica@be-tse.it>
2023-04-05 11:54:54 +0200

    feature: implement Functions + Vision API

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ import functions, { logger } from "firebase-functions";
 import vision from "@google-cloud/vision";
 import admin from "firebase-admin";
 
+admin.initializeApp();
+
 //Vediamo i cambiamenti dell'oggetto nel cloud storage bucket di default
 export const readReceiptDetails = functions.storage
   .object()
@@ -21,4 +23,23 @@ export const readReceiptDetails = functions.storage
     const text = annotation ? annotation.description : "";
     //Possiamo loggare il testo in Cloud Functions per vedere se é stato parsato correttamente
     logger.log(text);
+
+    // Parsa il testo
+    //Get uid
+    //object.name é userId/timestamp
+    const re = /(.*)\//;
+    const uid = re.exec(object.name)[1];
+    // Hardcode il testo ritornato per i campi rilevanti
+    const receipt = {
+      address: "Via Conte Di Ruvo, 76",
+      amount: "10,60",
+      date: new Date(),
+      imageBucket,
+      isConfirmed: false,
+      items: "2 caffé, 2 cornetti, 1 bottiglietta d'acqua",
+      locationName: "Caffé 76",
+      uid,
+    };
+
+    admin.firestore().collection("receipts").add(receipt);
   });
